0e17036af451c05669db9b953bffd101404d9294
nathanmanesh <nathanmanesh2@gmail.com>
2026-01-18 15:52:37 -0800

    nathan's version of submit()

--- task2/confidentiality.py
+++ task2/confidentiality.py
@@ -3,23 +3,40 @@
 from task1.CBC import *
 from task1.EBC import *
 
+from Crypto.Random import get_random_bytes
 
-def submit(string: str):
-    prepend = "userid=456; userdata="
-    append = ";session-id=31337"
+from urllib.parse import quote
 
-    print (prepend + string + append)
-    
+AES_BLOCK_SIZE = 16  # bytes (128 bits) for AES
+
+KEY = get_random_bytes(16)
+
+PREFIX = "userid=456;userdata="
+SUFFIX = ";session-id=31337"
+
+def submit(string: str) -> bytes:
 
 
+    # URL encode any ‘;’ and ‘=’ characters that appear in the user provided string; 
+    safe_string = quote(string, safe="")
 
-    
+    # prepend and append to user provided string
+    message_str = PREFIX + safe_string + SUFFIX
+
+    # convert to bytes for crypto
+    message = message_str.encode("utf-8")
+
+    # pad for PKCS#7
+    padded = pad_pkcs7(message, AES_BLOCK_SIZE)
+
+    ciphertext = ecb_encrypt(padded, KEY)
+    return ciphertext
+
 
 def verify():
     pass
 
 
-submit("You're the man now, dog")
 
 
 
